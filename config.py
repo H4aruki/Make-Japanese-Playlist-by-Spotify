8466136e247dac59df869bd9ca1f4f6ab190701b
K.Haruki <K.H4aruki@outlook.jp>
2025-09-08 20:08:17 +0900

    run test at local

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 #情報の取得
+
 import os
 
 # GitHub Secretsから環境変数を読み込む
@@ -6,7 +7,7 @@ CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 REDIRECT_URI = "http://127.0.0.1:8888/callback"   #自身を表す世界共通のアドレス
 
-SCOPE = "user-library-read playlist-read-private playlist-read-public playlist-modify-private playlist-modify-public"
+SCOPE = "user-library-read playlist-read-private playlist-modify-public playlist-modify-private"
 
 playlist_id = os.getenv("PLAYLIST_ID")   #お気に入りプレイリストのid
 Japanese_playlist_id = os.getenv("JAPANESE_PLAYLIST_ID")   #日本語曲専用のプレイリストid
